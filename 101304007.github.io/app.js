392b19fcaa1ae43871d278b9e1a0a8865be111fb
許仲廷 <101304007@nccu.edu.tw>
2015-04-08 01:07:13 +0800

    Initial commit

--- 101304007.github.io/app.js
+++ 101304007.github.io/app.js
@@ -1,118 +1,124 @@
 $(function() {
-    var currrentText = '';
+    var firstText = '';
     var temp = 0;
-    var situation;
+    var condition;
 
     $('#btn1').on('click', function() {
-        currrentText = currrentText + '1';
+        firstText = firstText + '1';
         render();
     });
 
     $('#btn2').on('click', function() {
-        currrentText = currrentText + '2';
+        firstText = firstText + '2';
         render();
     });
 
     $('#btn3').on('click', function() {
-        currrentText = currrentText + '3';
+        firstText = firstText + '3';
         render();
     });
 
     $('#btn4').on('click', function() {
-        currrentText = currrentText + '4';
+        firstText = firstText + '4';
         render();
     });
 
     $('#btn5').on('click', function() {
-        currrentText = currrentText + '5';
+        firstText = firstText + '5';
         render();
     });
 
     $('#btn6').on('click', function() {
-        currrentText = currrentText + '6';
+        firstText = firstText+ '6';
         render();
     });
 
     $('#btn7').on('click', function() {
-        currrentText = currrentText + '7';
+        firstText = firstText + '7';
         render();
     });
 
     $('#btn8').on('click', function() {
-        currrentText = currrentText + '8';
+        firstText = firstText + '8';
         render();
     });
 
     $('#btn9').on('click', function() {
-        currrentText = currrentText + '9';
+        firstText = firstText + '9';
         render();
     });
 
     $('#btn0').on('click', function() {
-        currrentText = currrentText + '0';
+        firstText = firstText + '0';
         render();
     });
 
     $('#btnAdd').on('click', function() {
-        situation = '+';
-        temp = currrentText;
+        condition = '+';
+        temp = firstText;
         calculator();
     });
 
     $('#btnMinus').on('click', function() {
-        situation = '-';
-        temp = currrentText;
+        condition = '-';
+        temp = firstText;
         calculator();
     });
 
     $('#btnMultiple').on('click', function() {
-        situation = '*';
-        temp = currrentText;
+        condition = '*';
+        temp = firstText;
         calculator();
     });
 
     $('#btnDivide').on('click', function() {
-        situation = '/';
-        temp = currrentText;
+        condition = '/';
+        temp = firstText;
         calculator();
     });
 
 
     $('#btnEqual').on('click', function() {
-        if (situation == '+') {
-            currrentText = parseFloat(temp) + parseFloat(currrentText);
-            $('#message').text(currrentText);
+        if (condition == '+') {
+            firstText = parseFloat(temp) + parseFloat(firstText);
+            $('#message').text(firstText);
         }
 
-        if (situation == '-') {
-            currrentText = parseFloat(temp) - parseFloat(currrentText);
-            $('#message').text(currrentText);
+        if (condition == '-') {
+            firstText = parseFloat(temp) - parseFloat(firstText);
+            $('#message').text(firstText);
         }
-        if (situation == '*') {
-            currrentText = parseFloat(temp) * parseFloat(currrentText);
-            $('#message').text(currrentText);
+        if (condition == '*') {
+            firstText = parseFloat(temp) * parseFloat(firstText);
+            $('#message').text(firstText);
         }
-        if (situation == '/') {
-            currrentText = parseFloat(temp) / parseFloat(currrentText);
-            $('#message').text(currrentText);
+        if (condition == '/') {
+            firstText = parseFloat(temp) / parseFloat(firstText);
+            $('#message').text(firstText);
         }
 
     });
 
     $('#btnClear').on('click', function() {
-        currrentText = '';
+        firstText = '';
         temp1 = 0;
-        situation = 0;
+        condition = 0;
+        render();
+    });
+    $("#btnAllclear").on('click',function(){
+        firstText = '';
+        temp1 = 0;
+        condition = 0;
         render();
     });
 
     function render() {
-        $('#message').text(currrentText);
+        $('#message').text(firstText);
     }
 
     function calculator() {
-        temp = currrentText;
-        currrentText = '';
+        temp = firstText;
+        firstText = '';
     }
 
 });
